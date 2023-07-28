5feec7d4f489333d6c184a2f4d4bd8ac38fb6012
Austin Papritz <austin@papritz.dev>
2023-07-28 09:58:14 -0700

    shared _Layout.cshtml now showing, removed error rerouting in Program.cs

--- HairSalon/Program.cs
+++ HairSalon/Program.cs
@@ -19,14 +19,6 @@ var app = builder.Build();
 // DEVELOPMENT ONLY
 DataInitializer.InitializeData(app);
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
