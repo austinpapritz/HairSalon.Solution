83ea0e1f8e2803b6eadc52116cf6b0ae9cca6aa8
Austin Papritz <austin@papritz.dev>
2023-07-28 10:33:18 -0700

    added OnModelCreating override to HairSalonContext

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -8,5 +8,19 @@ namespace HairSalon.Models
         public DbSet<Client>? Clients { get; set; }
 
         public HairSalonContext(DbContextOptions options) : base(options) { }
+
+        // Define the one-Stylist-to-many-Clients relationship using Fluent API
+        // This allows db migration using EntityFrameworkCore.Design
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Build the Stylist Entity
+            modelBuilder.Entity<Stylist>()
+                // Stylist HasMany Clients
+                .HasMany(s => s.Clients)
+                // Client only WithOne stylist
+                .WithOne(c => c.stylist)
+                // Identify a Client's Stylist with the StylistId fk
+                .HasForeignKey(c => c.StylistId);
+        }
     }
 }
