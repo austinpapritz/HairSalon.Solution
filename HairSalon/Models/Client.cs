ca03ab2d62ab719a73327e887304d8166aa8c4b7
Austin Papritz <austin@papritz.dev>
2023-07-28 10:20:50 -0700

    updated Style and Client classes with fields and db navigation

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,4 +2,8 @@ namespace HairSalon.Models;
 
 public class Client
 {
+    public int ClientId { get; set; }
+    public string Name { get; set; }
+    public int StylistId { get; set; }
+    public Stylist stylist { get; set; }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -2,4 +2,7 @@ namespace HairSalon.Models;
 
 public class Stylist
 {
+    public int StylistId { get; set; }
+    public string? Name { get; set; }
+    public ICollection<Client> Clients { get; set; }
 }
