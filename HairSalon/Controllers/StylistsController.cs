49aa9c746e506acdb32f2f2947891ec514dd9bb8
Austin Papritz <austin@papritz.dev>
2023-07-28 12:15:15 -0700

    client list rendering for each stylist

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -1,5 +1,6 @@
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace HairSalon.Controllers;
@@ -16,6 +17,26 @@ public class StylistsController : Controller
     public IActionResult Index()
     {
         List<Stylist> model = _db.Stylists.ToList();
+        return View(model);
+    }
+
+    public IActionResult Details(int id)
+    {
+        // Retrieve the currentStylist, including their Client list
+        Stylist currentStylist = _db.Stylists
+                                    .Include(s => s.Clients)
+                                    .FirstOrDefault(s => s.StylistId == id);
+
+        // If currentStylist returns null then return error
+        if (currentStylist == null)
+        {
+            return NotFound();
+        }
+
+        // Set model to list of the currentStylist's clients, set currentStylist to ViewBag
+        List<Client> model = currentStylist.Clients.ToList();
+        ViewBag.Stylist = currentStylist;
+
         return View(model);
     }
 }
