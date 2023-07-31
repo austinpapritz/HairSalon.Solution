91ac4a926010c97e7bc86c29d0cd4872808d02ca
Austin Papritz <austin@papritz.dev>
2023-07-31 14:11:36 -0700

    as of EF Core 5.0 you do not need to define relationship in context

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -8,25 +8,5 @@ namespace HairSalon.Models
         public DbSet<Client>? Clients { get; set; }
 
         public HairSalonContext(DbContextOptions options) : base(options) { }
-
-        // Define the one-Stylist-to-many-Clients relationship using Fluent API
-        // This allows db migration using EntityFrameworkCore.Design
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Build the Stylist Entity
-            modelBuilder.Entity<Stylist>()
-                // Wage can only have 2 decimal places.
-                .Property(s => s.Wage)
-                .HasPrecision(4, 2);
-
-            // Build the Stylist Entity
-            modelBuilder.Entity<Stylist>()
-                // Stylist HasMany Clients
-                .HasMany(s => s.Clients)
-                // Client only WithOne stylist
-                .WithOne(c => c.Stylist)
-                // Identify a Client's Stylist with the StylistId fk
-                .HasForeignKey(c => c.StylistId);
-        }
     }
 }
